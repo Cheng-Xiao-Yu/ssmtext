61bac8bc7bfcc067b98e41f3e5a2429ec7a2b678
王浩宇 <1270217552@qq.com>
2024-10-11 09:39:55 +0800

    通过Article类创建了一个文件

--- src/main/java/jdbc/Article.java
+++ src/main/java/jdbc/Article.java
@@ -1,4 +1,13 @@
 package jdbc;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Article {
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fos=new FileOutputStream("./demo.txt");
+        fos.write(1);
+        System.out.println("写出完毕");
+        fos.close();
+    }
 }
